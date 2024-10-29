8711e0e674c6734f971b6e4b5f9b1152b6b486c1
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-10-29 14:51:19 -0700

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = require("@ducanh2912/next-pwa").default({
+  dest: "public",
   output: "export", // Outputs a Single-Page Application (SPA).
   distDir: "./dist", // Changes the build output directory to `./dist/`.
   basePath: process.env.NEXT_PUBLIC_BASE_PATH, // Sets the base path to `/some-base-path`.
-};
+});
 
 export default nextConfig;
